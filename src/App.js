52422a8d263cbd9c6d781419d3489f9041c5b980
iruojefaith <fumsy240@gmail.com>
2023-01-08 23:30:03 +0100

    added tab

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import * as React from 'react';
 import './App.css';
 import Navbar from './NavBar/navbar';
-import Trending from './components/Search';
-import Search from './components/Trending';
-import Toprated from './components/Toprated';
+import Tab from './components/tab';
+import Search  from './components/Search';
+
+
 
 export default function App() {
   return (
     <div className="bg-black ">
     <Navbar />
      <Search />
-      <h1>Movie App </h1>
-
-      <Trending />
-      <h1>Toprated</h1>
-      <Toprated />
+      <Tab />
     </div>
   );
 }
---
+++ src/components/tab.js
@@ -0,0 +1,57 @@
+import React,{ useState } from 'react';
+import Toprated from './Toprated';
+import Trending from './Trending';
+
+const Tab = () => {
+   const tabStatus = ["Trending","TopRated","New"];
+    const [ openTab, setOpenTab ] = useState(1);
+
+  return (
+    <div className='flex justify-center align-center '>
+    <hr></hr>
+      <div className='w-full '>
+        <div className='flex justify-center align-center mt-3'>
+          <ul className='flex mb-0 list-none pt-3 pb-4 flex-row cursor-pointer ' role='tablist'>
+            {tabStatus.map((item, i) => {
+              return (
+                <li className=' text-center'>
+                  <a
+                    className={
+                      "text-xs md:text-lg font-normal px-8 py-3 shadow-lg  block leading-normal " +
+                      (openTab === i
+                        ? "text-white font-bold  " +
+                          "-600 "
+                        : "text-[#fff] " + "-600 ")
+                    }
+                    key={i}
+                    onClick={() => setOpenTab(i)}
+                  >
+                    {item}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className='relative flex flex-col min-w-0 break-words  w-full mb-6 shadow-lg rounded'>
+          <div className='px-4 py-5 flex-auto'>
+            <div className='tab-content tab-space'>
+              <div className=''>
+                {openTab === 0 ? (
+                  <Trending />
+                ) : openTab === 1 ? (
+                  <Toprated />
+                ) : (
+                  <Toprated />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Tab;
