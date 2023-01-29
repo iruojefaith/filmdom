28a8632879c5a7f25690204b299547dd2f172703
iruojefaith <faithiruoje@gmail.com>
2023-01-29 21:48:19 +0100

    made some changes

---
+++ src/components/context.js
@@ -0,0 +1,16 @@
+import React,{ useContext, createContext } from 'react'
+
+const AppContext = createContext();
+
+
+const AppProvider = ({ children })=>{
+  return (
+    <AppContext.Provider value={{}}>
+      {children}
+    </AppContext.Provider>
+  )
+};
+export const useGlobalCntext = ( ) => {
+  return useContext(AppContext);
+}
+export {AppContext, AppProvider};
